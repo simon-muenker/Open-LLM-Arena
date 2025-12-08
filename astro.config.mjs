99911ab7e8d7e2a916ce50dc438888db80e9ee88
Simon Münker <muenker@uni-trier.de>
2025-12-08 14:36:27 +0100

    add tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from 'astro/config'
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://simon-muenker.github.io',
   base: '/Open-LLM-Arena',
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
