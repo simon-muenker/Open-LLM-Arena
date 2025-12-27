70bcbde146d583fd2297c9c805563b0a626beb21
Simon Münker <muenker@uni-trier.de>
2025-12-27 11:19:58 +0100

    add models

--- src/assets/models.ts
+++ src/assets/models.ts
@@ -16,18 +16,33 @@ export const MODELS: Array<Model> = [
 		size: 229
 	},
 	{
+		name: "GPT OSS",
+		url: "openai/gpt-oss-120b",
+		size: 120
+	},
+	{
+		name: "Olmo 3.1",
+		url: "allenai/Olmo-3.1-32B-Instruct",
+		size: 32
+	},
+	{
 		name: "Ministral 3",
 		url: "mistralai/Ministral-3-14B-Instruct-2512",
 		size: 14
 	},
 	{
-		name: "Ministral 3",
-		url: "mistralai/Ministral-3-8B-Instruct-2512",
+		name: "Qwen 3",
+		url: "Qwen/Qwen3-VL-8B-Instruct",
 		size: 8
 	},
 	{
-		name: "Qwen 3",
-		url: "Qwen/Qwen3-VL-8B-Instruct",
+		name: "Olmo 3",
+		url: "allenai/Olmo-3-7B-Instruct",
+		size: 7
+	},
+	{
+		name: "Ministral 3",
+		url: "mistralai/Ministral-3-8B-Instruct-2512",
 		size: 8
 	},
 	{
